a65e856078740ccc8aaaf912b3a9d27c6918c357
Cyber Blaze <cyb3rblaz3@anshuls-mbp.attlocal.net>
2019-07-31 09:54:10 -0700

    Initial commit

---
+++ SRGAN.py
@@ -0,0 +1,171 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from os import listdir
+from tqdm import tqdm
+from PIL import Image
+
+#===========================
+#LOAD AND PREPROCESS IMAGES
+#===========================
+def save_training_data(directory):
+    list = listdir(directory)
+    images = []
+    size = (512, 512)
+
+    for image_name in tqdm(list):
+        image = Image.open(directory + "/" + image_name).convert("RGB").resize(size)
+        image.save("training_data/high_res/" + image_name)
+        image = image.resize((128, 128))
+        image = image.resize((512, 512))
+        image.save("training_data/low_res/" + image_name)
+
+def normalize(image):
+    image = tf.cast(image, tf.float32)
+    image = (image / 127.5) - 1
+    return image
+
+def load_and_preprocess_data(directory):
+    low_resList = listdir(directory + "/low_res")
+    high_resList = listdir(directory + "/high_res")
+
+    low_resImages = []
+    high_resImages = []
+
+    for image in tqdm(low_resList):
+        low_resImages += [normalize(np.array(Image.open(directory + "/low_res/" + image)).reshape((1, 512, 512, 3)))]
+        high_resImages += [normalize(np.array(Image.open(directory + "/high_res/" + image)).reshape((1, 512, 512, 3)))]
+
+    return low_resImages, high_resImages
+
+#===========================
+#CREATE MODELS
+#===========================
+
+def res_block():
+    pass
+
+def disc_block(filters, strides):
+    #initializer = tf.random_normal_initializer(0., 0.02)
+    result = tf.keras.Sequential()
+    #result.add(tf.keras.layers.Conv2D(filters, size, strides=2, padding='same', kernel_initializer=initializer, use_bias=False))
+    result.add(tf.keras.layers.Conv2D(filters, 3, strides=strides, padding='same', use_bias=False))
+    result.add(tf.keras.layers.BatchNormalization())
+    result.add(tf.keras.layers.LeakyReLU())
+
+    return result
+
+def create_generator():
+    pass
+
+def create_discriminator():
+    input = tf.keras.layers.Input(shape=[512, 512, 3])
+
+    conv1 = tf.keras.layers.Conv2D(64, 3, strides=1, padding='same')(input)
+    lrelu1 = tf.keras.layers.LeakyReLU()(conv1)
+
+    block1 = disc_block(64, 2)(lrelu1)
+    block2 = disc_block(128, 1)(block1)
+    block3 = disc_block(128, 2)(block2)
+    block4 = disc_block(256, 1)(block3)
+    block5 = disc_block(256, 2)(block4)
+    block6 = disc_block(512, 1)(block5)
+    block7 = disc_block(512, 2)(block6)
+
+    flatten = tf.keras.layers.Flatten()(block7)
+    dense1 = tf.keras.layers.Dense(1024)(flatten)
+    lrelu2 = tf.keras.layers.LeakyReLU()(dense1)
+    dense2 = tf.keras.layers.Dense(1, activation="sigmoid")(lrelu2)
+
+    return tf.keras.Model(inputs=input, outputs=dense2)
+
+discriminator = create_discriminator()
+#discriminator.summary()
+
+#===========================
+#LOSS FUNCTIONS
+#===========================
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+def generator_loss(fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+#===========================
+#CREATING MODEL OPTIMIZER OBJECTS
+#===========================
+def create_adam_optimizer():
+    return tf.keras.optimizers.Adam(1e-4)
+
+generator_optimizer = create_adam_optimizer()
+discriminator_optimizer = create_adam_optimizer()
+
+#===========================
+#FUNCTION USED TO SAVE DATA
+#===========================
+def saveEpochImage(model, test_input, epoch):
+    prediction = model(test_input, training=True)
+    plt.figure(figsize=(15,15))
+
+    display_list = [test_input[0], prediction[0]]
+    title = ['Input Image', 'Predicted Image']
+
+    for i in range(2):
+        plt.subplot(1, 2, i+1)
+        plt.title(title[i])
+        plt.imshow(display_list[i] * 0.5 + 0.5)
+        plt.axis('off')
+    plt.savefig("./output/" + str(epoch) + ".jpg")
+
+#===========================
+#TRAIN MODEL
+#===========================
+EPOCHS = 150
+
+def one_train_step(input, target):
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        gen_output = generator(input, training=True)
+
+        disc_real_output = discriminator(target, training=True)
+        disc_generated_output = discriminator(gen_output, training=True)
+
+        gen_loss = generator_loss(disc_generated_output)
+        disc_loss = discriminator_loss(disc_real_output, disc_generated_output)
+
+    gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+
+    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+def train(low_resData, high_resData, epochs, seed):
+    saveEpochImage(generator, seed, 0)
+
+    for epoch in tqdm(range(EPOCHS)):
+        for low, high in tqdm(zip(low_resData, high_resData)):
+            train_step(low, high)
+
+        if epoch % 5 == 0:
+            saveEpochImage(generator, seed, (epoch+1))
+
+#===========================
+#FUNCTION CALLS
+#===========================
+#save_training_data("./raw_data")
+print("Beginning train data preprocessing...")
+low_res, high_res = load_and_preprocess_data("./training_data")
+
+print("Beginning training...")
+
+    #===========================
+    #CREATING SEED TO SEE PROGRESS
+    #===========================
+seed = low_res[0]
+
+train(low_res, high_res, EPOCHS, seed)
